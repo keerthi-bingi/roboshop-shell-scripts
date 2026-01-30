5dc5ef4704b4dc79149141d885f157e2c39ee6fb
keerthizendesk <keerthi.bingi@zendesk.com>
2026-01-30 19:17:51 +0530

    Adding scripts

--- catalogue.sh
+++ catalogue.sh
@@ -7,6 +7,8 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+SCRIPT_DIR="$PWD"
+MONGODB_HOST=mongodb.daws88s.online
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
